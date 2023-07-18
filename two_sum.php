35d4641b26435f05cfaf07f73e0176dffe39eb79
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-07-18 04:46:36 +0300

    improve two sum problem

--- two_sum.php
+++ two_sum.php
@@ -3,27 +3,21 @@
 
      function twoSum($num,$target){
         $number1 = 0;
-        $number1Index = 0;
         $number2 = 0;
-        $number2Index = 0;
-
         for ($i = 0;$i < count($num);$i++){
             $number1 = $num[$i];
             for($j = 0;$j < count($num) ; $j++){
                 $number2 = $num[$j];
 
                 if (($number1 + $number2) == $target && $i != $j){
-                    $number1Index = $i;
-                    $number2Index = $j;
-
-                    return [$number1Index,$number2Index];
+                    return [$i,$j];
                 }
             }
 
         }
     }
 
-    $sum = twoSum([3,3],6);
+    $sum = twoSum([3,5,8,3],6);
 
      print_r($sum); ;
 
